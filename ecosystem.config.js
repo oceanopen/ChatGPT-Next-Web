ad1f94b099c69b8060d92aa8a3bdb3b220378883
ocean-gao <gaopandyx@163.com>
2024-12-30 00:50:41 +0800

    ci: pm2 增加 HOSTNAME 配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,7 @@ module.exports = {
       exec_mode: "cluster",
       env: {
         NODE_ENV: "production",
+        HOSTNAME: "0.0.0.0",
       },
       output: "./logs/app_output.log",
       error: "./logs/app_error.log",
